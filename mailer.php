20edf89d7c29de9e325ae59dd10063f902ea1e84
Alessandro Capra <alessandro.cpr@gmail.com>
2018-02-11 15:26:00 +0100

    Fixed email sending

--- mailer.php
+++ mailer.php
@@ -28,10 +28,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_content .= "Message:\n$message\n";
 
     // Build the email headers.
-    // $email_headers = "From: $name <$email>";
+    $email_headers = "From: $name <$email>";
 
     // Send the email.
-    if (mail($recipient, $subject, $email_content)) { // optionally add $email_headers
+    if (mail($recipient, $subject, $email_content, $email_headers)) { // optionally add $email_headers
         // Set a 200 (okay) response code.
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
